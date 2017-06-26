134a60300f80c1bfae78a3c964381e8a46afdeca
limesimmunogenomics <limesimmunogenomics@gmail.com>
2017-06-26 16:50:45 +0200

    Update install.packages.R

--- rstudio_extended/install.packages.R
+++ rstudio_extended/install.packages.R
@@ -1,7 +1,9 @@
 
 source('https://bioconductor.org/biocLite.R')
+biocLite("GenomicRanges")
+
 install.packages(c("tidyverse","dplyr","ggplot2","devtools","formatR","data.table","ggExtra","matrixStats","scales",
-"useful","grid","data.table","plotly","RColorBrewer","reshape2","plyr","VennDiagram","zoo","stringr","ggrepel","geneplotter",
+"useful","grid","plotly","RColorBrewer","reshape2","plyr","VennDiagram","zoo","stringr","ggrepel","geneplotter",
 "RColorBrewer","VennDiagram","colorspace","fdrtool"),keep_outputs=TRUE)
 
 # bioconductor packages
