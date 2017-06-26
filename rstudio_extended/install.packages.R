056e9cc95cce9f411f659d3ffc4311d219843ef4
limesimmunogenomics <limesimmunogenomics@gmail.com>
2017-06-26 16:04:40 +0200

    Create install.packages.R

---
+++ rstudio_extended/install.packages.R
@@ -0,0 +1,35 @@
+
+source('https://bioconductor.org/biocLite.R')
+install.packages(c("tidyverse","dplyr","ggplot2","devtools","formatR","data.table","ggExtra","matrixStats","scales",
+"useful","grid","data.table","plotly","RColorBrewer","reshape2","plyr","VennDiagram","zoo","stringr","ggrepel","geneplotter",
+"RColorBrewer","VennDiagram","colorspace","fdrtool"))
+
+# bioconductor packages
+biocLite("monocle")
+biocLite("DeSeq2")
+biocLite("biomaRt")
+biocLite("BioGenerics")
+biocLite("BiocInstaller")
+biocLite("BiocParralell")
+biocLite("Chipseeker")
+biocLite("clusterProfiler")
+biocLite("DOSE")
+biocLite("Gviz")
+biocLite("h5")
+biocLite("IRanges")
+biocLite("limma")
+biocLite("WGCNA")
+biocLite("pcaGoPromoter")
+biocLite("pcaGoPromoter.Mm.mm9")
+biocLite("pcaGoPromoter.Hs.hg19")
+biocLite("org.Mm.eg.db")
+biocLite("org.Hs.eg.db")
+biocLite("WGCNA")
+biocLite("WGCNA")
+biocLite("WGCNA")
+biocLite("WGCNA")
+
+# github packages
+ install_github("satijalab/seurat")
+
+
