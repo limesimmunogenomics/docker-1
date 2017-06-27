090485b2974a86a03abff383a9d5bf970acb0385
limesimmunogenomics <limesimmunogenomics@gmail.com>
2017-06-27 09:04:25 +0200

    Update install.packages.R

--- rstudio_extended/install_scripts/install.packages.R
+++ rstudio_extended/install_scripts/install.packages.R
@@ -2,3 +2,30 @@ source('https://bioconductor.org/biocLite.R')
 install.packages(c("tidyverse","dplyr","ggplot2","devtools","formatR","data.table","ggExtra","matrixStats","scales",
 "useful","grid","data.table","plotly","RColorBrewer","reshape2","plyr","VennDiagram","zoo","stringr","ggrepel","geneplotter",
 "RColorBrewer","VennDiagram","colorspace","fdrtool"),keep_outputs=TRUE)
+
+
+ 
+ # bioconductor packages
+ biocLite("monocle")
+ biocLite("DeSeq2")
+ biocLite("biomaRt")
+ biocLite("BioGenerics")
+ biocLite("BiocInstaller")
+ biocLite("BiocParralell")
+ biocLite("Chipseeker")
+ biocLite("clusterProfiler")
+ biocLite("DOSE")
+ biocLite("Gviz")
+ biocLite("h5")
+ biocLite("IRanges")
+ biocLite("limma")
+ biocLite("WGCNA")
+ biocLite("pcaGoPromoter")
+ biocLite("pcaGoPromoter.Mm.mm9")
+ biocLite("pcaGoPromoter.Hs.hg19")
+ biocLite("org.Mm.eg.db")
+ biocLite("org.Hs.eg.db")
+ biocLite("geneplotter")
+  
+ # github packages
+ install_github("satijalab/seurat")
